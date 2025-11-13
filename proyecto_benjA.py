8066e6c0ff7cda33f742c6c86d8c6a4e37edf261
JosepMartiVillalonga <martivillalongajosep@gmail.com>
2025-11-13 12:04:49 +0100

    Sobreescritura de archivos añadidos a partir de un archivo

--- proyecto_benjA.py
+++ proyecto_benjA.py
@@ -229,13 +229,38 @@ def page_3():
                             st.error("❌ Introduce un nombre de rival válido antes de guardar.")
                         else:
                             ruta_guardado = CARPETA_PARTIDOS/nombre_guardado
-                            if ruta_guardado.exists():
+                            st.session_state['archivo_a_guardar'] = {
+                                'ruta': str(ruta_guardado),
+                                'df' : df_editado.to_dict(orient='list')
+                            }
+                            if Path(ruta_guardado).exists():
                                 st.warning(f"⚠️ Ya existe un archivo llamado '{ruta_guardado.name}'. Se sobrescribirá al guardar.")
+                                st.session_state['mostrar_confirmacion'] = True
+                            else:
+                                df_partido.to_csv(ruta_guardado, index=False, encoding="utf-8")
+                                st.success(f"✅ Archivo actualizado correctamente en '{ruta_guardado.name}'")
+                                st.session_state.pop("mostrar_confirmacion", None)
+                                st.session_state.pop("archivo_a_guardar", None)
+                                st.rerun()
+                    if st.session_state.get("mostrar_confirmacion", False):
+                                ruta_guardado = Path(st.session_state["archivo_a_guardar"]["ruta"])
+                                col1, col2 = st.columns(2)
+                                with col1:
+                                    if st.button("✅ Sobrescribir archivo"):
+                                        df_guardar = pd.DataFrame(st.session_state["archivo_a_guardar"]["df"])
+                                        df_guardar.to_csv(ruta_guardado, index=False, encoding="utf-8")
+                                        st.success(f"✅ Archivo sobrescrito correctamente en '{ruta_guardado.name}'")
+                                        st.session_state.pop("archivo_a_guardar", None)
+                                        st.session_state.pop("mostrar_confirmacion", None)
+                                        st.rerun()
                                 
-                            df_editado.to_csv(ruta_guardado, index=False, encoding="utf-8")
-                            st.success(f"✅ Archivo actualizado correctamente en '{ruta_guardado.name}'")
-                            st.rerun()
-
+                                with col2: 
+                                    if st.button("❌ Cancelar"):
+                                        st.info("❌ Operación cancelada. El archivo no fue modificado.")
+                                        st.session_state.pop("archivo_a_guardar", None)
+                                        st.session_state.pop("mostrar_confirmacion", None)
+                                        st.rerun()
+                                
 
                 else:
                     guardar = st.button("📦 Guardar partido sin editar")
